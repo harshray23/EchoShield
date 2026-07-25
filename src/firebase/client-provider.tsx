2b7630727c45769a9179ad231c352b22be6abc4a
Harsh Agarwalla <pappua088@gmail.com>
2026-07-25 19:02:18 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,17 +1,25 @@
 'use client';
 
 import React, { useMemo } from 'react';
-import { initializeFirebase } from './index';
+import { getFirebaseApp, getFirebaseAuth, getFirebaseFirestore } from './config';
 import { FirebaseProvider } from './provider';
 
 /**
  * FirebaseClientProvider handles the initialization of Firebase on the client side.
  * This avoids serialization errors when passing Firebase instances from Server to Client components.
+ * It imports directly from config to avoid circular dependencies with the index barrel file.
  */
 export const FirebaseClientProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const { firebaseApp, firestore, auth } = useMemo(() => initializeFirebase(), []);
+  const { firebaseApp, firestore, auth } = useMemo(() => {
+    const app = getFirebaseApp();
+    return {
+      firebaseApp: app,
+      auth: getFirebaseAuth(app),
+      firestore: getFirebaseFirestore(app),
+    };
+  }, []);
 
   return (
     <FirebaseProvider firebaseApp={firebaseApp} firestore={firestore} auth={auth}>
